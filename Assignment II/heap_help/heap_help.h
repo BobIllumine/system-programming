e9eb74efad60aeb650092d3fee9f5ed3d86fd6a1
BobIllumine <heredisstultus@gmail.com>
2023-03-13 18:07:29 +0300

    testing parser

---
+++ Assignment II/heap_help/heap_help.h
@@ -0,0 +1,11 @@
+#pragma once
+//#define _GNU_SOURCE
+
+
+#include <stdint.h>
+
+void
+heaph_init(void);
+
+uint64_t
+heaph_get_alloc_count(void);
--- Assignment II/main.c
+++ Assignment II/main.c
@@ -1,6 +1,58 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include "heap_help/heap_help.h"
+
+typedef struct command {
+    const char *name;
+    char *const *argv;
+    const int argc;
+} cmd_t;
+
+void exec_cmd(cmd_t command) {
+    pid_t pid = fork();
+    if(pid == -1) {
+        printf("Shit's bad");
+    }
+    else if(pid == 0) {
+        if(execvp(command.name, command.argv) < 0) {
+            printf("Something went bad");
+        }
+        exit(0);
+    }
+    else {
+        wait(NULL);
+        return;
+    }
+}
+
+void exec_piped_cmd(cmd_t cmd1) {}
+
+cmd_t* parse_pipe(char *str) {
+    cmd_t *commands = NULL;
+    char delim[] = "|";
+    char *full_str = str;
+    char *token = NULL;
+    while((token = strsep(&full_str, delim)) != NULL) {
+        printf("%s, ", token);
+    }
+    free(token);
+    return commands;
+}
 
 int main() {
-    printf("Hello, World!\n");
+    heaph_init();
+    char *hello_world = NULL;
+    size_t len = 0;
+    ssize_t read;
+    while((read = getline(&hello_world, &len, stdin)) != -1) {
+        cmd_t *result = parse_pipe(hello_world);
+    }
+    free(result);
+    free(hello_world);
+    printf("%lu", heaph_get_alloc_count());
     return 0;
 }
